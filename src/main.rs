6098d5829ec030b34e111baa2d910f9f6f34aaba
shiveen <shiveen.pandita@ordermentum.com>
2019-03-13 21:40:33 +1100

    add parsing to camel case

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-extern crate pest;
 #[macro_use]
 extern crate pest_derive;
 
 use pest::prec_climber::{Assoc, Operator, PrecClimber};
 use serde_json::json;
 use std::env;
+use serde::Serialize;
 
 use pest::Parser;
 
@@ -12,6 +12,19 @@ use pest::Parser;
 #[grammar = "rrule.pest"]
 struct RRuleParser;
 
+#[derive(Serialize)]
+#[serde(rename_all = "camelCase")]
+struct RRule<'a> {
+    frequency: String,
+    count: String,
+    interval: String,
+    by_hour: Vec<&'a str>,
+    by_minute: Vec<&'a str>,
+    by_second: Vec<&'a str>,
+    by_day: Vec<&'a str>,
+    by_month_day: Vec<&'a str>,
+    by_year_day: Vec<&'a str>,
+}
 
 /// error occurred when parsing user input
 #[derive(Debug)]
@@ -20,25 +33,88 @@ pub struct ParseError {
     pub expected: String,
 }
 
+use pest::iterators::Pair;
+use std::collections::HashMap;
+
 /// converts and rrule string to a jsonified response
 fn convert_rrule_to_json(
     rrule_string: String,
 ) {
+    let mut rrule_result = RRule {
+        frequency: String::from(""),
+        count: String::from(""),
+        interval: String::from(""),
+        by_hour: Vec::new(),
+        by_minute: Vec::new(),
+        by_second: Vec::new(),
+        by_day: Vec::new(),
+        by_month_day: Vec::new(),
+        by_year_day: Vec::new(),
+    };
+
     let parse_result = RRuleParser::parse(Rule::expr, rrule_string.as_str())
         .expect("unsuccessful parse") // unwrap the parse result
         .next().unwrap();
 
-    for line in parse_result.into_inner() {
+    for mut line in parse_result.into_inner() {
         match line.as_rule() {
             Rule::freq_expr => {
                 let mut inner_rules = line.into_inner();
                 let this_rule = inner_rules.next().unwrap().as_str();
-                println!("result is {}", this_rule)
+                rrule_result.frequency = this_rule.to_string()
             }
 
+            Rule::interval_expr => {
+                let mut inner_rules = line.into_inner();
+                let this_rule = inner_rules.next().unwrap().as_str();
+                rrule_result.interval = this_rule.to_string()
+            }
+
+            Rule::count_expr => {
+                let mut inner_rules = line.into_inner();
+                let this_rule = inner_rules.next().unwrap().as_str();
+                rrule_result.count = this_rule.to_string()
+            }
+
+            Rule::byhour_expr => {
+                let mut inner_rules = line.into_inner();
+                let this_rule = inner_rules.next().unwrap().as_str().split(",").collect();
+                rrule_result.by_hour = this_rule
+            }
+
+            Rule::byminute_expr => {
+                let mut inner_rules = line.into_inner();
+                let this_rule = inner_rules.next().unwrap().as_str().split(",").collect();
+                rrule_result.by_minute = this_rule
+            }
+
+            Rule::bysecond_expr => {
+                let mut inner_rules = line.into_inner();
+                let this_rule = inner_rules.next().unwrap().as_str().split(",").collect();;
+                rrule_result.by_second = this_rule
+            }
+
+            Rule::byday_expr => {
+                let mut inner_rules = line.into_inner();
+                let this_rule = inner_rules.next().unwrap().as_str().split(",").collect();
+                rrule_result.by_day = this_rule
+            }
+
+            Rule::bymonthday_expr => {
+                let mut inner_rules = line.into_inner();
+                let this_rule = inner_rules.next().unwrap().as_str().split(",").collect();;
+                rrule_result.by_month_day = this_rule
+            }
+
+            Rule::byyearday_expr => {
+                let mut inner_rules = line.into_inner();
+                let this_rule = inner_rules.next().unwrap().as_str().split(",").collect();;
+                rrule_result.by_year_day = this_rule
+            }
             _ => {}
         }
     }
+    println!("Json is {}", serde_json::to_string(&rrule_result).unwrap());
 }
 
 fn main() {
