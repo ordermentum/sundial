432a74567cb598e3df5a3d21ff9d89ee1875b5d9
John D'Agostino <john@ordermentum.com>
2019-11-22 14:23:56 +1100

    update and use serde for options

--- sundial_wasm/src/lib.rs
+++ sundial_wasm/src/lib.rs
@@ -1,16 +1,25 @@
+use sundial::get_all_iter_dates;
+use serde::{Serialize, Deserialize};
 use wasm_bindgen::prelude::*;
-use sundial::{get_all_iter_dates};
+
+#[derive(Serialize, Deserialize)]
+pub struct SundialOptions {
+    pub rule: String,
+    pub count: String,
+    pub until: String,
+}
 
 #[wasm_bindgen]
-pub fn get_dates() -> Option<Box<[JsValue]>> {
-    let rrule = "FREQ=DAILY;COUNT=4;INTERVAL=1;BYDAY=WE;BYHOUR=9;BYMINUTE=1;DTSTART=20190327T030000;TZID=Australia/Brisbane";
-
+pub fn get_dates(value: &JsValue) -> JsValue {
     let mut rrules_result_native = vec![];
-
-    match get_all_iter_dates(&rrule, "10", "") {
+    let options: SundialOptions = value.into_serde().unwrap();
+    match get_all_iter_dates(&options.rule.to_owned(), &options.count.to_owned(), &options.until.to_owned()) {
         Ok(rrules) => rrules_result_native = rrules,
-        Err(err) => return None,
+        Err(_) => return JsValue::NULL,
     }
 
-    Some(vec![JsValue::NULL].into_boxed_slice())
+    match JsValue::from_serde(&rrules_result_native) {
+        Ok(value) => value,
+        Err(_) => JsValue::NULL
+    }
 }
